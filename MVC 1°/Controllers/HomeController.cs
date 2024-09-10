ca9cd8bdcf7be9ba2d74276b3f1eacd4dc3a42ac
Lautaro Gaston Vijande <lautarovijande@itisjt.edu.ar>
2024-09-09 21:40:24 -0300

    Cambios en Program para manegar sesiones y Layout para cambios en cambios de error en layout de sesiones

--- MVC 1°/Controllers/HomeController.cs
+++ MVC 1°/Controllers/HomeController.cs
@@ -8,14 +8,14 @@ namespace MVC_1_.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-       
+
         DbProyectoContext db = new DbProyectoContext();
 
-        public IActionResult SignUp() 
+        public IActionResult SignUp()
         {
             return View();
         }
-        
+
         [HttpPost]
         public IActionResult SignUp(Usuario usuario)
         {
@@ -44,7 +44,49 @@ namespace MVC_1_.Controllers
 
         public IActionResult Index()
         {
+            return View(db.Usuarios.ToList());
+        }
+
+        
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpGet]
+        public ActionResult LogIn()
+        {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult LogIn(Usuario usuario)
+        {
+            var checkLogin = db.Usuarios.Where(x => x.Email.Equals(usuario.Email)
+                && x.Clave.Equals(usuario.Clave))
+                    .FirstOrDefault();
+
+            if (checkLogin != null)
+            {
+                HttpContext.Session.SetString("email", usuario.Email.ToString());
+                HttpContext.Session.SetString("clave", usuario.Clave.ToString());
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.Notification = "Email o Clave incorrecta";
+            }
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index", "Home");
         }
 
         public IActionResult Privacy()
@@ -52,10 +94,5 @@ namespace MVC_1_.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
--- MVC 1°/Program.cs
+++ MVC 1°/Program.cs
@@ -3,10 +3,26 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-var app = builder.Build();
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+
+});
+
+var app = builder.Build();//COnfigurar la app
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
@@ -18,10 +34,13 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
+
+app.UseSession();// Habilita la session
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=SignUp}/{id?}");
+    pattern: "{controller=Home}/{action=SingUp}/{id?}");
 
 app.Run();
